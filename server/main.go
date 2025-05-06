62f4059aaebacfbc94257145d58702ecfa89fc66
gokul <gokulviswanathan25@gmail.com>
2025-05-06 17:58:03 -0400

    Cors setup complete. AI call implemented to backend

--- server/main.go
+++ server/main.go
@@ -3,14 +3,64 @@ package main
 import (
 	// "fmt"
 	// "net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gokul-viswanathan/note-taker/server/handlers"
 )
 
+// complete the cors setup
 func main() {
 	router := gin.Default()
-	router.GET("/airesponse", handlers.PromptHandler)
+	router.Use(corsMiddleware())
 
+	router.GET("/airesponse", handlers.PromptHandler)
+	// CORS config
+	// config := cors.Config{
+	// 	AllowOrigins:     []string{"*"},
+	// 	AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	// 	AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+	// 	ExposeHeaders:    []string{"Content-Length"},
+	// 	AllowCredentials: true,
+	// 	MaxAge:           12 * time.Hour,
+	// }
 	router.Run("localhost:8080")
+}
+
+// CORS middleware function definition
+func corsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		originsString := "https://example.com,https://test.com,http://192.168.1.44:3000,http://localhost:3000"
+		allowedOrigins := strings.Split(originsString, ",")
+
+		for i, origin := range allowedOrigins {
+			allowedOrigins[i] = strings.TrimSpace(origin)
+		}
+
+		isOriginAllowed := func(origin string, allowedOrigins []string) bool {
+			for _, allowedOrigin := range allowedOrigins {
+				if origin == allowedOrigin {
+					return true
+				}
+			}
+			return false
+		}
+
+		origin := c.Request.Header.Get("Origin")
+		c.Writer.Header().Set("Vary", "Origin")
+
+		if isOriginAllowed(origin, allowedOrigins) {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+			c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+			c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		}
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
