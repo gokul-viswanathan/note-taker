596a51747299ef2a32976553e64032314fb85631
gokul <gokulviswanathan25@gmail.com>
2025-08-28 10:49:48 -0400

    updating file content on pushing to git
    added push to git button.
    current file get updated

--- client/src/components/NewQuillEditor.tsx
+++ client/src/components/NewQuillEditor.tsx
@@ -1,19 +1,20 @@
 'use client';
 import React, { forwardRef, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import Quill from 'quill';
+import { Op } from 'quill-delta';
 const Delta = Quill.import('delta');
 import 'quill/dist/quill.snow.css'; // Import Quill's default snow theme
 import toolbarOptions from '@/services/quillToolBar'
 import { useStore } from '@/stores/states'
 import fetchFileContent from '@/services/getFileContent';
-
+import updateFileContent from '@/services/updateFileContent'
 /* 
  * TODO: 
  * 1. data should be visibile - of current file -> working
- * 2. data should be saved on text-change as object or local localStorage
- * 3. api call to get the data on currentfile change
- * 4. chnaging the file or closing the file should make the post request to save the data
- * 5. multiple editor instance should be handled
+ * 2. data should be saved on text-change as object or local localStorage - working
+ * 3. api call to get the data on currentfile change - working
+ * 4. chnaging the file or closing the file should make the post request to save the data - not implemented
+ * 5. multiple editor instance should be handled - completed
  * */
 
 
@@ -22,9 +23,23 @@ const Editor = forwardRef((props, forwardedRef): React.JSX.Element => {
     const containerRef = useRef<HTMLDivElement>(null);
     const quillRef = useRef<Quill | null>(null);
     const currentFile = useStore((state) => state.currentFile);
+    const [fileContent, setFileContent] = useState<Op[] | null>(null);
+    const setCurrentFileContent = useStore((state) => state.setCurrentFileContent);
+    const saveFile = useStore((state) => state.saveFile);
+    const setSaveFile = useStore((state) => state.setSaveFile);
+
+    useEffect(() => {
+        if (quillRef.current || !containerRef.current) return;
+        quillRef.current = new Quill(containerRef.current, { theme: 'snow', modules: { toolbar: toolbarOptions } });
+
+        return () => {
+            if (containerRef.current) containerRef.current.innerHTML = '';
+        };
+    }, []);
 
     useEffect(() => {
         const loadFileContent = async () => {
+
             if (currentFile && typeof currentFile !== 'string' && 'path' in currentFile) {
                 const data = await fetchFileContent(currentFile.path);
                 if (data && quillRef.current) {
@@ -43,23 +58,17 @@ const Editor = forwardRef((props, forwardedRef): React.JSX.Element => {
         loadFileContent();
     }, [currentFile]);
 
-    useEffect(() => {
-        if (quillRef.current || !containerRef.current) return;
-        quillRef.current = new Quill(containerRef.current, { theme: 'snow', modules: { toolbar: toolbarOptions } });
-
-        return () => {
-            if (containerRef.current) containerRef.current.innerHTML = '';
-        };
-    }, []);
-
     //another useEffect for quill changes
     useEffect(() => {
+        //WARNING: updating content directly to current file will cause too many rerenders
+        //TODO: why not call api based on timeout - use debounce and sepeartion on content change and file change
+
         const handleTextChange = () => {
-            console.log("Text changed, saving content to localStorage");
-            localStorage.setItem(
-                "fileContent",
-                JSON.stringify(quillRef.current?.getContents().ops)
-            );
+            if (quillRef.current) {
+                const content = quillRef.current.getContents();
+                setFileContent(content.ops || []);
+                setCurrentFileContent && setCurrentFileContent(content.ops || []);
+            }
         };
         quillRef.current?.on("text-change", handleTextChange);
 
@@ -68,6 +77,21 @@ const Editor = forwardRef((props, forwardedRef): React.JSX.Element => {
         };
 
     }, [quillRef.current]);
+
+    // if a button is clicked push the content to api
+    useEffect(() => {
+        if (saveFile && currentFile && typeof currentFile !== 'string' && 'path' in currentFile) {
+            const saveContent = async () => {
+                await updateFileContent(currentFile, useStore.getState().currentFileContent)
+            }
+            saveContent()
+        }
+
+    }, [saveFile]);
+
+    useEffect(() => {
+        console.log("Content updated in zustand:", useStore.getState().currentFileContent);
+    }, [fileContent]);
 
     return <div ref={containerRef}></div>;
 });
