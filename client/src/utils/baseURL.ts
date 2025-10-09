f25090a6ac7ce28e7edde082712aaec36dde1b12
gokul <gokulviswanathan25@gmail.com>
2025-10-09 00:16:34 -0400

    callback url updated in env variables

--- client/src/utils/baseURL.ts
+++ client/src/utils/baseURL.ts
@@ -2,4 +2,7 @@ const getBaseUrl = (): string => {
   return process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
 };
 
+export const callBackUrl = (): string => {
+  return process.env.NEXT_PUBLIC_CALLBACK_URL || "http://localhost:3000";
+};
 export default getBaseUrl;
